3ae514cb3af3cc25123bd25356f1e71ff532aaf2
HamnaShafiq <hamnashafiq012@gmail.com>
2025-01-08 22:57:25 +0500

    try

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-const express = require("express");
-const cors = require('cors');
-
-const app = express();
-
-require("dotenv").config();
-require("./config/db").connect();
-
-app.use(cors())
-app.use(express.static(__dirname + '/public'));
-app.use(express.json());
-
-const apiRoutes = require("./routes/index");
-
-app.use("/api", apiRoutes);
-
-module.exports = app;
-
-
-
 // const express = require("express");
 // const cors = require('cors');
 
@@ -25,22 +5,8 @@ module.exports = app;
 
 // require("dotenv").config();
 // require("./config/db").connect();
-// require('./models/product');
 
-// // app.use(cors())
-
-// const corsoption ={
-//     origin: [
-//         "http://localhost:3000",
-//         "https://departmental-store-frontend.vercel.app/"
-//         ],
-//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//     credentials: true,
-// }
-// app.use(cors(corsoption));
-
-
-// app.options("*", cors(corsoption));
+// app.use(cors())
 // app.use(express.static(__dirname + '/public'));
 // app.use(express.json());
 
@@ -49,3 +15,37 @@ module.exports = app;
 // app.use("/api", apiRoutes);
 
 // module.exports = app;
+
+
+
+const express = require("express");
+const cors = require('cors');
+
+const app = express();
+
+require("dotenv").config();
+require("./config/db").connect();
+require('./models/product');
+
+// app.use(cors())
+
+const corsoption ={
+    origin: [
+        "http://localhost:3000",
+        "https://departmental-store-frontend.vercel.app"
+        ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+}
+app.use(cors(corsoption));
+
+
+app.options("*", cors(corsoption));
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+
+const apiRoutes = require("./routes/index");
+
+app.use("/api", apiRoutes);
+
+module.exports = app;
