f6a07f347d50578a3b5f040d1d881358543ec7de
HamnaShafiq <hamnashafiq012@gmail.com>
2025-01-08 21:02:50 +0500

    aai

--- app.js
+++ app.js
@@ -5,22 +5,8 @@ const app = express();
 
 require("dotenv").config();
 require("./config/db").connect();
-require('./models/product');
 
-// app.use(cors())
-
-const corsoption ={
-    origin: [
-        "http://localhost:3000",
-        "https://departmental-store-frontend.vercel.app/"
-        ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    credentials: true,
-}
-app.use(cors(corsoption));
-
-
-app.options("*", cors(corsoption));
+app.use(cors())
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 
